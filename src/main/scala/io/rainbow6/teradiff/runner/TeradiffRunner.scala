55e7891e0e0c0856e75db6805191b676a0b3eed5
wpengyu1505 <wpengyu1505@gmail.com>
2018-11-18 00:28:55 -0500

    Adding bash script

--- src/main/scala/io/rainbow6/teradiff/runner/TeradiffRunner.scala
+++ src/main/scala/io/rainbow6/teradiff/runner/TeradiffRunner.scala
@@ -27,7 +27,7 @@ object TeradiffRunner {
     }
 
     val conf = new SparkConf().setAppName("TeraDiff")//.setMaster("local")
-    val spark = SparkSession.builder.config(conf).getOrCreate()
+    val spark = SparkSession.builder.config(conf).config("spark.sql.warehouse.dir", "/user/hive/warehouse/").enableHiveSupport().getOrCreate()
     spark.sql("set spark.sql.shuffle.partitions=%s".format(partitions))
 
     var df1 = null:DataFrame
